c081a7765ec5200ff61bac79dcd2cb69c3d4d128
canxue <1419761729@qq.com>
2020-05-13 17:30:05 +0800

    init

---
+++ tabbar/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/home.svg">
+        <div>主页</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/category.svg">
+        <div>分类</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/shopcart.svg">
+        <div>购物车</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/profile.svg">
+        <div>我的</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+
+</style>
